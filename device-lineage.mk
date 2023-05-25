b2e2413625a23b60fbbbce9f90f1d435b828633e
Matt Filetto <matt.filetto@gmail.com>
2023-05-25 01:33:45 -0700

    lynx: add AlphaDroid specific build flags
    Signed-off-by: Matt Filetto <matt.filetto@gmail.com>

--- device-lineage.mk
+++ device-lineage.mk
@@ -19,6 +19,23 @@ persist.sys.aux.camera_oem_package=com.google.android.GoogleCamera
 PRODUCT_PACKAGES += \
     HbmSVManagerOverlayLynx
 
+# GMS
+WITH_GAPPS := 2
+TARGET_CORE_GAPPS := false
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+TARGET_USE_GOOGLE_TELEPHONY := true
+
+# AlphaDroid Specific Build Properties
+TARGET_HAS_UDFPS := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_ENABLE_BLUR := true
+ALPHA_MAINTAINER := Albinoman887
+TARGET_INCLUDE_MATLOG := true
+TARGET_USE_PIXEL_LAUNCHER := false
+TARGET_EXCLUDES_AUDIOFX := false
+
+# We don't use the PE Implementation of Face Unlock
+TARGET_FACE_UNLOCK_SUPPORTED := false
 
 # PowerShare
 include hardware/google/pixel/powershare/device.mk
